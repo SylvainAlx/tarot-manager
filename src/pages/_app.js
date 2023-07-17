aca644635db48e1313dae0cd523fa41e09c89368
SylvainAlx <sylvainalexandre@gmail.com>
2023-07-17 19:02:32 +0200

    update Head

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
 import "../styles/styles.scss";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Tarot Manager</title>
+        <meta
+          name="description"
+          content="Gestionnaire de classement pour le tarot"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,15 +11,6 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <title>Tarot Manager</title>
-        <meta
-          name="description"
-          content="Gestionnaire de classement pour le tarot"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Header />
       <main>
         <h2>Bienvenue</h2>
